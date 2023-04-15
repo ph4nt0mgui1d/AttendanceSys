eea39590c85adfd2160a853e302af6b8d4df5464
ph4nt0mgui1d <aryansharma.bkn@gmail.com>
2023-04-15 16:46:33 +0530

    Updates

--- src/pages/Report.js
+++ src/pages/Report.js
@@ -7,118 +7,139 @@ import Highlighter from 'react-highlight-words';
 const Report = () => {
   const [showTable, setShowTable] = useState(false)
   const [tableColumns, setTableColumns] = useState()
-  // const [searchText, setSearchText] = useState('');
-  // const [searchedColumn, setSearchedColumn] = useState('');
-  // const searchInput = useRef(null);
+  const [loadReport, setLoadReport] = useState([])
+  // FILTER BEGIN
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
 
-  // const handleSearch = (selectedKeys, confirm, dataIndex) => {
-  //   confirm();
-  //   setSearchText(selectedKeys[0]);
-  //   setSearchedColumn(dataIndex);
-  // };
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
 
-  // const handleReset = (clearFilters) => {
-  //   clearFilters();
-  //   setSearchText('');
-  // };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
 
-  // const getColumnSearchProps = (dataIndex) => ({
-  //   filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
-  //     <div
-  //       style={{
-  //         padding: 8,
-  //       }}
-  //       onKeyDown={(e) => e.stopPropagation()}
-  //     >
-  //       <Input
-  //         ref={searchInput}
-  //         placeholder={`Search ${dataIndex}`}
-  //         value={selectedKeys[0]}
-  //         onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-  //         onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-  //         style={{
-  //           marginBottom: 8,
-  //           display: 'block',
-  //         }}
-  //       />
-  //       <Space>
-  //         <Button
-  //           type="primary"
-  //           onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-  //           icon={<SearchOutlined />}
-  //           size="small"
-  //           style={{
-  //             width: 90,
-  //           }}
-  //         >
-  //           Search
-  //         </Button>
-  //         <Button
-  //           onClick={() => clearFilters && handleReset(clearFilters)}
-  //           size="small"
-  //           style={{
-  //             width: 90,
-  //           }}
-  //         >
-  //           Reset
-  //         </Button>
-  //         <Button
-  //           type="link"
-  //           size="small"
-  //           onClick={() => {
-  //             confirm({
-  //               closeDropdown: false,
-  //             });
-  //             setSearchText(selectedKeys[0]);
-  //             setSearchedColumn(dataIndex);
-  //           }}
-  //         >
-  //           Filter
-  //         </Button>
-  //         <Button
-  //           type="link"
-  //           size="small"
-  //           onClick={() => {
-  //             close();
-  //           }}
-  //         >
-  //           close
-  //         </Button>
-  //       </Space>
-  //     </div>
-  //   ),
-  //   filterIcon: (filtered) => (
-  //     <SearchOutlined
-  //       style={{
-  //         color: filtered ? '#1890ff' : undefined,
-  //       }}
-  //     />
-  //   ),
-  //   onFilter: (value, record) =>
-  //     record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
-  //   onFilterDropdownOpenChange: (visible) => {
-  //     if (visible) {
-  //       setTimeout(() => searchInput.current?.select(), 100);
-  //     }
-  //   },
-  //   render: (text) =>
-  //     searchedColumn === dataIndex ? (
-  //       <Highlighter
-  //         highlightStyle={{
-  //           backgroundColor: '#ffc069',
-  //           padding: 0,
-  //         }}
-  //         searchWords={[searchText]}
-  //         autoEscape
-  //         textToHighlight={text ? text.toString() : ''}
-  //       />
-  //     ) : (
-  //       text
-  //     ),
-  // });
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1890ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+  // FILTER END
+
   
-  let loadReport = []
+  async function getData(month) {
+    let loadR = []
+    const response = await fetch(`http://192.168.1.21/apicrudphp/api/report.php?month=${month}`);
+    const data = await response.json();
+    console.log(data)
+    for(const key in data){
+      loadR.push({name: data[key].name})
+      // for(let c = 1; c <= data.length; c++){
+        // for(let k=1; k<=31; k++){
+        // loadR.push({a: k})
+        // }
+      // }
+    }
+    setLoadReport(loadR);
+  }
+
+  
   const days = [31,28,31,30,31,30,31,31,30,31,30,31]
+
   const handleMenuClick = (e) => {
     setShowTable(true)
     const monthVal = e.key
@@ -126,36 +147,23 @@ const Report = () => {
       title: "Name",
       dataIndex: "name",
       key: "name",
-      width: "20%",
-      // ...getColumnSearchProps('name')
+      width: "30%",
+      ...getColumnSearchProps('name')
     },]
 
     for(let i = 1; i<=days[monthVal-1]; i++){
       tabC.push(
         {
           title: i,
-          dataIndex: 'a' + i,
+          dataIndex: 'a',
           key: 'k' + i,
           width: "1%",
         },
       );
       setTableColumns(tabC)
-      // getData(i);
     }
-
+    getData(monthVal);
     
-  
-    async function getData(date) {
-      const response = await fetch("url");
-      const data = response.json();
-      
-      // for(const key in data){
-      //   loadReport.push({name: data[key].name})
-      //   for(const k = 1; k<32; k++){
-      //     loadReport.push[data[key].k]
-      //   }
-      // }
-    }
   };
 
   //DropDown items
@@ -216,7 +224,7 @@ const Report = () => {
     <Dropdown menu={{items, onClick: handleMenuClick}} style={{width:"20%", marginBottom: '10px'}}>
       <Button>
         <Space>
-          Button
+          Month
           <DownOutlined />
         </Space>
       </Button>
